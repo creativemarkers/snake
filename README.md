7587963935e6285a9735c3997ed105624579eda4
Fernando Sanchez <nerudasrs@gmail.com>
2024-03-07 18:41:24 -0500

    changed name of the file running the game to game.py and updated readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 my first try at creating snake with python,  will only work on a terminal
 
 game is now complete and only has three built in python dependencies, os(to avoid screen shaking), time and random
+
+to run the game open your terminal, go to the directory the game is stored in and run game.py
--- game.py
+++ game.py
@@ -1,26 +1,48 @@
-"""
-this file will need to be refactored
-"""
+import time
+import os
+from map import Map
+from snake import Snake
+from input import Input
+from cookie import Cookie
 
+def clear_screen():
+    #to avoid jarring shaking visual from terminal call it at the beggining of the while loop
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+def main():
 
-gameOver = False
-winState = False
-drawMap
-drawSnake
-drawCookie
+    gameOver = False
+    mapSize = 20
+    m = Map(mapSize)
+    snake = Snake(mapSize)
+    cookie = Cookie(mapSize, snake.snake)
+    input = Input()
+    while gameOver == False:
+        clear_screen()
+        m.printSnake(snake.snake)
+        m.printCookie(cookie.currentPosition)
+        m.printMap()
+        m.deleteSnake(snake.snake)
+        
+        snake.moveSnakeHead(input.get_input(0.2))
 
-while not gameOver:
+        time.sleep(0.01)
 
-    #the game is over if the snake head touches any of it's body
-    if snake.isTouchingBody == True:     
-        gameOver = True:
-        take input
-            calculate next draw
-                print input on screen
-                    check if snake kills it self(gameOver) or picks up cookie
-                        check if last cookie, if it is gameOver and player Wins
-                        if not last cookie
-                            end this current loop
+        if snake.checkIfSnakeAteItself() == True:
+            print("You ate yourself!")
+            gameOver = True
 
+        if snake.outOfBounds() == True:
+            print("You went out of Bounds!")
+            gameOver = True
 
+        if snake.ateCookie(cookie.currentPosition) == True:
+            m.deleteCookie(cookie.currentPosition)
+            snake.grow()
+            cookie = Cookie(mapSize, snake.snake)
+        
+        time.sleep(0.05)
+
+    print("GameOver, you ate %s cookies!" % snake.cookiesEaten)
+
+main()
